bd9952c59783a931ba98e4d9b6d916a84f6ecb76
Troglodyte <troglodyte.contact@gmail.com>
2023-02-25 19:23:25 +0100

    1.8.5

--- js/code.js
+++ js/code.js
@@ -1,6 +1,5 @@
 import {C2J2_PATH} from "./variables.js";
 
-const ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const SPECIAL_CHARS = {
     "\"" : "quot",
     "'"  : "apos",
@@ -20,219 +19,183 @@ const ICONS = {
     "html" : ["html-5", "HTML 5 logo"]
 }
 
-function getSpan(color, value) {
-    return `<span class="fs-sm txt-${color}">${value}</span>`;
-}
-
 export function compute(element) {
     let html = "";
     let language = element.dataset.lang;
     let code = element.textContent;
-    if (language && LANGUAGES.includes(language)) {
+    let existing_language = language && LANGUAGES.includes(language);
+    if (existing_language) {
         if (!(element.dataset.hideHead === "true")) {
             html += `<div class="bd-rad-t-md pd-md d-flex gap-sm algn-i-center bg-light-grey"><img src="${C2J2_PATH}/img/${ICONS[language][0]}.svg" alt="${ICONS[language][1]}" style="max-width: 40px; max-height: 40px;">`;
             if (element.dataset.name) {html += `<p class="fs-sm txt-black">${element.dataset.name}</p>`;}
             html += "</div>"
         }
-        let list = [];
+    }
+    let bd = "bd-rad-b-md";
+    if (!existing_language || element.dataset.hideHead == "true") {bd = "bd-rad-md";}
+    html += `<div class="${bd} pd-md bg-dark-grey txt-w-break-breakall">`;
+    if (element.id && element.dataset.allowCopy === "true") {
+        html += `
+            <img id="${element.id}-copy" class="float-right mg-l-xs" src="${C2J2_PATH}/img/copy-to-clipboard.svg" alt="Copier" style="max-width: 40px; max-height: 40px;">
+            <p class="tool-tip bd-rad-sm pd-sm txt-w-space-nowrap bg-light-yellow txt-black" data-target="${element.id}-copy">Copier le code</p>
+        `;
+    }
+    let lines = [];
+    if (existing_language) {
         if (language == "html") {
-            let type = "out";
-            let value = "";
+            lines = [[]]
+            let type = "";
+            let color = "";
             for (let i of code) {
-                switch (type) {
-                    case "out":
-                        if (i == "<") {
-                            list.push(["white", value]);
-                            type = "sin";
-                            value = i;
-                        }
-                        else {value += i;}
-                        break;
-                    case "sin":
-                        if (i == "!") {
-                            type = "cin";
-                            value += i;
-                        }
-                        else if (i == "/" && value == "<") {value += i;}
-                        else if (i == " " || ALPHA.includes(i)) {
-                            list.push(["grey", value]);
-                            type = "tag";
-                            value = i;
-                        }
-                        else {
-                            list.push(["red", value[0]], ["white", value.slice(1)]);
-                            if (i != "<") {type = "out";}
-                            value = i;
-                        }
-                        break;
-                    case "tag":
-                        if (i == " ") {
-                            list.push(["light-blue", value]);
-                            type = "in";
-                            value = i;
-                        }
-                        else if (i == ">") {
-                            list.push(["light-blue", value], ["grey", i]);
-                            type = "out";
-                            value = "";
-                        }
-                        else {value += i;}
-                        break;
-                    case "in":
-                        if (i == ">") {
-                            list.push(["white", value], ["grey", i]);
-                            type = "out";
-                            value = "";
-                        }
-                        else if (i == " ") {value += i;}
-                        else {
-                            list.push(["white", value]);
-                            if (i == "-" || ALPHA.includes(i)) {type = "attr";}
-                            else {type = "err";}
-                            value = i;
-                        }
-                        break;
-                    case "attr":
-                        if (i == " " || i == "=") {
-                            list.push(["cyan", value]);
-                            type = "equ";
-                            value = i;
-                        }
-                        else if (i == ">") {
-                            list.push(["cyan", value], ["grey", i]);
-                            type = "out";
-                            value = "";
-                        }
-                        else if (i == "-" || ALPHA.includes(i)) {value += i;}
-                        else {
-                            list.push(["cyan", value]);
-                            type = "err";
-                            value = i;
-                        }
-                        break;
-                    case "equ":
-                        if (i == " " || i == "=" && !value.includes("=")) {value += i;}
-                        else {
-                            list.push(["white", value]);
-                            if (i == '"' && value.includes("=")) {type = "str1";}
-                            else if (i == "'" && value.includes("=")) {type = "str2";}
-                            else {type = "err";}
-                            value = i;
-                        }
-                        break;
-                    case "str1":
-                        value += i;
-                        if (i == '"' && !(value.length > 1 && value[value.length-2] == "\\")) {
-                            list.push(["orange", value]);
-                            type = "in";
-                            value = "";
-                        }
-                        break;
-                    case "str2":
-                        value += i;
-                        if (i == "'" && !(value.length > 1 && value[value.length-2] == "\\")) {
-                            list.push(["orange", value]);
-                            type = "in";
-                            value = "";
-                        }
-                        break;
-                    case "err":
-                        if (i == " ") {
-                            list.push(["red", value]);
-                            type = "in";
-                            value = i;
-                        }
-                        else if (i == ">") {
-                            list.push(["red", value], ["grey", i]);
-                            type = "out";
-                            value = i;
-                        }
-                        else {value += i;}
-                        break;
-                    case "cin":
-                        value += i;
-                        if (value == "<!--") {
-                            list.push(["dark-green", value]);
-                            type = "com";
-                            value = "";
-                        }
-                        else if (!("<!--".includes(value))) {
-                            list.push(["red", value[0]]);
-                            if (i == "<") {
-                                list.push(["white", value].slice(1, value.length-1));
-                                type = "sin";
-                                value = i;
-                            }
-                            else {
-                                list.push(["white", value.slice(1)]);
-                                type = "out";
-                                value = "";
-                            }
-                        }
-                        break;
-                    case "com":
-                        if (i == "-") {
-                            list.push(["dark-green", value]);
-                            type = "cout";
-                            value = i;
-                        }
-                        else {value += i;}
-                        break;
-                    case "cout":
-                        value += i;
-                        if (value == "-->") {
-                            list.push(["dark-green", value]);
-                            type = "out";
-                            value = "";
-                        }
-                        else if (!("-->".includes(value))) {
-                            if (i == "-") {
-                                list.push(["dark-green", value[0]]);
-                                value = value.slice(1);
-                            }
-                            else {
-                                list.push(["dark-green", value]);
-                                type = "com"
-                                value = "";
-                            }
-                        }
-                        break;
+                if (i == "\n") {
+                    lines.push([]);
+                    color = "";
                 }
-            }
-            let bd = "bd-rad-b-md";
-            if (element.dataset.hideHead == "true") {bd = "bd-rad-md";}
-            html += `<div class="${bd} pd-md bg-dark-grey txt-w-break-breakall">`;
-            if (element.id && element.dataset.allowCopy === "true") {
-                html += `
-                    <img id="${element.id}-copy" class="float-right mg-l-xs" src="${C2J2_PATH}/img/copy-to-clipboard.svg" alt="Copier" style="max-width: 40px; max-height: 40px;">
-                    <p class="tool-tip bd-rad-sm pd-sm txt-w-space-nowrap bg-light-yellow txt-black" data-target="${element.id}-copy">Copier le code</p>
-                `;
-            }
-            if (list.length != 0) {
-                let value = "";
-                for (let i = 0; i < list.length; i++) {
-                    value += list[i][1];
-                    if (value.length != 0 && (i == list.length-1 || list[i][0] != list[i+1][0])) {
-                        let j = 0;
-                        while (j < value.length) {
-                            if (value[j] == "\n") {
-                                value = value.slice(0, j)+"<br>"+value.slice(j+1);
-                                j += 4;
-                            }
-                            else if (value[j] in SPECIAL_CHARS) {
-                                value = value.slice(0, j)+"&"+SPECIAL_CHARS[value[j]]+";"+value.slice(j+1);
-                                j += SPECIAL_CHARS[value[j]].length+2;
-                            }
-                            else {j++;}
+                else if (type == "com") {
+                    color = "dark-green";
+                    if (i == ">") {
+                        let value = lines[lines.length-1][lines[lines.length-1].length-1][0];
+                        if (value.length >= 2 && value.slice(value.length-2, value.length) == "--") {
+                            type = "out";
                         }
-                        html += getSpan(list[i][0], value);
-                        value = "";
                     }
                 }
+                else if (i == " ") {
+                    color = "white";
+                    if (type == "tag") {type = "var";}
+                }
+                else if (i == "=") {
+                    color = "white";
+                    type = "equ";
+                }
+                else if (i == "\"") {
+                    color = "orange";
+                    if (type == "val") {type = "var";}
+                    else {type = "val";}
+                }
+                else if ("<>/!-".includes(i)) {
+                    if (type == "val") {color = "orange";}
+                    else {
+                        color = "grey";
+                        if (type == "tag") {type = "out";}
+                        else {type = "spe";}
+                    }
+                    if (i == "-" && lines[lines.length-1].length != 0) {
+                        let value = lines[lines.length-1][lines[lines.length-1].length-1][0];
+                        if (value.length >= 3 && value.slice(value.length-3, value.length) == "<!-") {
+                            color = "dark-green";
+                            lines[lines.length-1][lines[lines.length-1].length-1][1] = color;
+                            type = "com";
+                        }
+                    }
+                }
+                else {
+                    if (type == "out") {color = "white";}
+                    else if (type == "spe") {type = "tag";}
+                    else if (type == "var") {color = "cyan";}
+                    if (type == "tag") {color = "light-blue";}
+                }
+                if (color != "") {
+                    if (lines[lines.length-1].length == 0 || color != lines[lines.length-1][lines[lines.length-1].length-1][1]) {lines[lines.length-1].push(["", color]);}
+                    lines[lines.length-1][lines[lines.length-1].length-1][0] += i;
+                }
             }
-            html += "</div>";
         }
     }
+    else {
+        lines = element.textContent.split("\n");
+        for (let i = 0; i < lines.length; i++) {lines[i] = [[lines[i], "white"]];}
+    }
+    for (let i = 0; i < lines.length; i++) {
+        let j = 0;
+        while (j < lines[i].length-1) {
+            if (lines[i][j][0].length == 0) {lines[i] = lines[i].slice(1);}
+            else if (lines[i][j+1][1] == lines[i][j][1]) {
+                lines[i][j][0] += lines[i][j+1][0];
+                lines[i] = lines[i].slice(0, j+1)+lines[i].slice(j+2);
+            }
+            else {j++;}
+        }
+        if (lines[i].length != 0 && lines[i][lines[i].length-1][0].length == 0) {lines[i] = lines[i].slice(0, lines[i].length-1);}
+    }
+    let run = true;
+    while (run) {
+        let i = 0;
+        let j = 0;
+        while (run && i < lines[0].length) {
+            if (lines[0][i][0][j] != " ") {run = false;}
+            else {
+                j++;
+                if (j == lines[0][i][0].length) {
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+        if (run) {lines.shift();}
+    }
+    run = true;
+    while (run) {
+        let i = 0;
+        let j = 0;
+        while (run && i < lines[lines.length-1].length) {
+            if (lines[lines.length-1][i][0][j] != " ") {run = false;}
+            else {
+                j++;
+                if (j == lines[lines.length-1][i][0].length) {
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+        if (run) {lines.pop();}
+    }
+    if (lines.length != 0) {
+        let min_spaces = 999;
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i].length != 0) {
+                let nb_spaces = 0;
+                let j = 0;
+                while (j < lines[i][0][0].length && lines[i][0][0][j] == " ") {
+                    nb_spaces++;
+                    j++;
+                }
+                if (nb_spaces < min_spaces) {min_spaces = nb_spaces;}
+            }
+        }
+        for (let i = 0; i < lines.length; i++) {
+            if (i != 0) {html += "<br>";}
+            if (lines[i].length != 0) {
+                lines[i][0][0] = lines[i][0][0].slice(min_spaces);
+                for (let j = 0; j < lines[i].length; j++) {
+                    let k = 0;
+                    while (k < lines[i][j][0].length) {
+                        if (lines[i][j][0][k] == " ") {
+                            let l = k+1;
+                            while (l < lines[i][j][0].length && lines[i][j][0][l] == " ") {l++;}
+                            l -= k;
+                            if (l > 1) {
+                                let spaces = "";
+                                for (let m = 0; m < parseInt(l/2); m++) {spaces += " &nbsp;";}
+                                if (l%2) {spaces += " ";}
+                                lines[i][j][0] = lines[i][j][0].slice(0, k)+spaces+lines[i][j][0].slice(k+l);
+                                k += l+parseInt(l/2)*5;
+                            }
+                            else {k++;}
+                        }
+                        else if (lines[i][j][0][k] in SPECIAL_CHARS) {
+                            let special_char = SPECIAL_CHARS[lines[i][j][0][k]];
+                            lines[i][j][0] = lines[i][j][0].slice(0, k)+"&"+special_char+";"+lines[i][j][0].slice(k+1)
+                            k += special_char.length+2;
+                        }
+                        else {k++;}
+                    }
+                    if (lines[i][j][0].length != 0) {html += `<span class="fs-sm txt-${lines[i][j][1]}">${lines[i][j][0]}</span>`;}
+                }
+            }
+        }
+    }
+    html += "</div>";
     element.innerHTML = html;
     if (element.id && element.dataset.allowCopy === "true") {
         let copy_button = document.getElementById(element.id+"-copy");
